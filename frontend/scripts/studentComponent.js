819351c781624dd650c50632c674b53deb202e0e
Karina2409 <karina.siardziuk@mail.ru>
2024-11-12 15:18:12 +0300

    fix: move the code from studentcomponent to another file

--- frontend/scripts/studentComponent.js
+++ frontend/scripts/studentComponent.js
@@ -1,7 +1,5 @@
 //TODO: Изменить текст в элементе, заменить его на значения, полученные с сервера
 
-let students = [];
-
 function createStudentCard(student, eventStudents) {
     const studentWrapper = document.createElement("div");
     studentWrapper.classList.add("student-item");
@@ -23,46 +21,6 @@ function createStudentCard(student, eventStudents) {
         </div>
     `
     return studentWrapper;
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    const list = document.querySelector('.students-list');
-    if (list) {
-        fetch('../../data/students.json')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => {
-                students = data;
-                renderStudentList(list, students);
-            })
-            .catch(error => {
-                console.error('Ошибка загрузки файла:', error);
-            });
-    }
-})
-
-function renderStudentList(list, students) {
-    list.innerHTML = '';
-    const button = document.querySelector('.show-more');
-    if (students.length > 13) {
-        button.classList.add('visible');
-    }
-    let i = 1;
-
-    while (i <= 13) {
-        students.forEach(student => {
-            if (i <= 13) {
-                const eventStudents = generateEventsCount(student.events_id.length);
-                const card = createStudentCard(student, eventStudents);
-                list.append(card);
-                i++;
-            }
-        })
-    }
 }
 
 function openStudentInfoPage(student) {
@@ -92,22 +50,4 @@ function generateEventsCount(eventsCount) {
     }
 
     return studentEventCount;
-}
-
-function addStudentsCard() {
-    const list = document.querySelector('.students-list');
-
-    const button = document.querySelector('.show-more');
-    button.classList.remove('visible');
-
-    let i = 1;
-    students.forEach(student => {
-        if (i > 13) {
-            const eventStudents = generateEventsCount(student.events_id.length);
-            const card = createStudentCard(student, eventStudents);
-            list.append(card);
-        }
-        i++;
-    })
-
 }
