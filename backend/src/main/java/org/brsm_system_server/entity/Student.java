411a7f9d2a381a0e7af1f9cad447334645ad9555
Karina2409 <karina.siardziuk@mail.ru>
2024-11-22 18:58:30 +0300

    fix: add columns to student and secretary files

--- backend/src/main/java/org/brsm_system_server/entity/Student.java
+++ backend/src/main/java/org/brsm_system_server/entity/Student.java
@@ -2,6 +2,7 @@ package org.brsm_system_server.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.brsm_system_server.entity.enums.FacultyEnum;
 
 import java.util.Set;
 
@@ -22,5 +23,44 @@ public class Student {
             inverseJoinColumns = @JoinColumn(name = "events_event_id")
     )
     private Set<Event> events;
+
+    @ManyToMany(mappedBy = "students")
+    private Set<Exemption> exception;
+
+    @Column(name="student_full_name_d")
+    private String studentFullNameD;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @Column (name = "group_number")
+    private String groupNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "student_faculty")
+    private FacultyEnum studentFaculty;
+
+    @Column(name = "dormitory_residence")
+    private boolean dormitoryResidence;
+
+    @Column(name = "dorm_block_number")
+    private String dormBlockNumber;
+
+    @Column(name = "dorm_number")
+    private Integer dormNumber;
+
+    @ManyToMany
+    @JoinTable(
+            name = "student_report",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "report_id")
+    )
+    private Set<Report> reports;
 
 }
